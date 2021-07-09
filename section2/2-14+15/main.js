96d0cefe267d213df784841a3317b3ebf60f99f7
RyoKitatani <ryo0929m@gmail.com>
2021-07-09 11:43:29 +0900

    finish 2-15 v-bind

---
+++ section2/2-14+15/main.js
@@ -0,0 +1,14 @@
+new Vue({
+  el: '#app',
+  data: {
+    url: "https://google.com:",
+    number: 31,
+    attribute: "href",
+    twitterObject: {
+      href: "https://twitter.com:",
+      id: 31
+    }
+  },
+})
+
+// オブジェクトしてまめることも可能
