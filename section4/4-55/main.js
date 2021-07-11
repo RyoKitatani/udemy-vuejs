825a6a2a5b30c54a39d584f549cad08241c6c1a9
RyoKitatani <ryo0929m@gmail.com>
2021-07-11 13:13:05 +0900

    finish 4-55

---
+++ section4/4-55/main.js
@@ -0,0 +1,19 @@
+var data = {
+  message: 'こんにちは',
+  name: 'よしぴー'
+}
+
+var vm = new Vue({
+  el: '#app',
+  data: data,
+  computed: {
+    myData: function() {
+      return this.$data;
+    }
+  }
+})
+
+
+console.log(data == vm.$data);
+console.log(vm);
+console.log(vm.$data);
