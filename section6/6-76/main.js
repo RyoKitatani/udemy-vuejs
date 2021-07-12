b22c230ce6ad740f7d19f47f559c55e2cb023155
RyoKitatani <ryo0929m@gmail.com>
2021-07-12 15:41:22 +0900

    finish 6-76

---
+++ section6/6-76/main.js
@@ -0,0 +1,19 @@
+
+
+Vue.component('my-component', {
+  data: function() {
+    return {
+      number :12
+    }
+  },
+  template: '<p>いいね({{number}})<button @click="increment">+1</button></p>',
+  methods: {
+    increment: function() {
+      this.number += 1;
+    }
+  }
+})
+
+new Vue({
+  el:'#app'
+})
